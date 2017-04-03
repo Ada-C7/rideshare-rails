2824fda9b9e6fdaa9eeafb6980d24ba307d02458
Leesyruiz <leesyruiz@gmail.com>
2017-04-03 16:19:35 -0700

    initilized project/created models

---
+++ db/migrate/20170403230855_create_trips.rb
@@ -0,0 +1,13 @@
+class CreateTrips < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trips do |t|
+
+      t.belongs_to :rider
+      t.belongs_to :driver
+      
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
