53f801fe6759ee262245c1f88f59e6e728daaa28
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-04-03 16:26:56 -0700

    added Create New Rider function

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -2,4 +2,21 @@ class RidersController < ApplicationController
   def index
     @riders = Rider.all
   end
+
+  def new
+    @rider = Rider.new
+  end
+
+  def create
+    Rider.create(rider_params)
+
+    redirect_to riders_path
+  end
+
+  private
+
+  def rider_params
+    return params.require(:rider).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'riders', to: 'riders#index', as: 'riders'
+get 'riders/new', to: 'riders#new', as: 'new_rider'
+post 'riders', to: 'riders#create'
+
 end
