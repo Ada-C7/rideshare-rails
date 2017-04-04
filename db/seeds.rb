b8a11f0e05f8360eaee26461f8c0a0a32a630333
laura <lmelgarejos@unal.edu.co>
2017-04-04 15:51:03 -0700

    Put Driver's data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,24 @@
 
 require 'csv'
 
-csv_text = File.read(Rails.root.join('support', 'passengers.csv'))
+# csv_text = File.read(Rails.root.join('support', 'passengers.csv'))
+#
+# csv = CSV.parse(csv_text, :headers => true)
+# csv.each do |row|
+#   r = Rider.new
+#   r.id = row['rider_id']
+#   r.name = row['name']
+#   r.phone_num = row["phone_num"]
+#   r.save
+# end
+
+csv_text = File.read(Rails.root.join('support', 'drivers.csv'))
 
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
-  r = Rider.new
-  r.id = row['rider_id']
+  r = Driver.new
+  r.id = row['driver_id']
   r.name = row['name']
-  r.phone_num = row["phone_num"]
+  r.vin = row["vin"]
   r.save
 end
