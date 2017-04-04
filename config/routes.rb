0d8c743796000d9dd7f6e9036ae6d47ba7bcc48d
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-04-04 14:49:29 -0700

    added driver

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-resources :riders
+resources :riders, :drivers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # get 'riders', to: 'riders#index', as: 'riders'
