af0bf7fd3e6775cf2154558f8621ab265ae4b35b
Ann Dai <gofarann@gmail.com>
2017-04-04 14:53:56 -0700

    adjusted index view for riders

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'riders/index'
+  root "riders#index"
+  resources :riders
 
-  root "trips#index"
+  root "trips#index" #root defaults to first one
   resources :trips
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
