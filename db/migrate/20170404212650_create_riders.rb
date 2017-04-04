1dc300009f0c2a0a3133d357b6806c58114cc18e
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-04 14:35:49 -0700

    Created schema w/ db:migrate

--- db/migrate/20170404212650_create_riders.rb
+++ db/migrate/20170404212650_create_riders.rb
@@ -1,7 +1,9 @@
 class CreateRiders < ActiveRecord::Migration[5.0]
   def change
     create_table :riders do |t|
-
+      t.string :name
+      t.string :phone_num
+      
       t.timestamps
     end
   end
