cf6fb293c8fe080625428aac074edbd9120dc716
natalia-ku <bilberryfm@mail.ru>
2017-04-04 14:51:56 -0700

    added index,create,destroy methods to controller

--- app/controllers/rider_controller.rb
+++ app/controllers/rider_controller.rb
@@ -1,22 +1,37 @@
 class RiderController < ApplicationController
     def index
+      @riders = Rider.all
     end
 
     def new
+      @task = Rider.new
     end
 
     def create
+      Rider.create(rider_params)
     end
 
     def show
+      @rider = Rider.find(params[:id])
     end
 
     def edit
+      @rider = Rider.find(params[:id])
     end
 
     def update
+      rider = Rider.find(params[:id])
+      rider.update_attributes(rider_params)
+      rider.save
     end
 
     def destroy
+      @rider = Rider.find(params[:id])
+      @rider.destroy
+    end
+
+    private
+    def rider_params
+      return params.require(:rider).permit(:name, :phone_num)
     end
 end
