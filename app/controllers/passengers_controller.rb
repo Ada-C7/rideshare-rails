641195c09b33f6086e6e25224eceeac1b9d1afd1
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-04 15:51:40 -0700

    created passengers and drivers controller

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,24 @@
+class PassengersController < ApplicationController
+
+
+  def index
+    @passengers = Passenger.all
+  end
+
+
+  def create
+    Passenger.create(passenger_params)
+    redirect_to passengers_path
+  end
+
+  def show
+    @passenger = Passenger.find(params[:id])
+  end
+
+  private
+  def passenger_params
+    return params.require(:passenger).permit(:id, :name, :phone_num)
+  end
+
+
+end
