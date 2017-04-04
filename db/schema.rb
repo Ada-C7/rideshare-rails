9483e08c0254dfaaf2293da5a1e75b38d890cff2
Anacelia Gillette <acgillette426@gmail.com>
2017-04-04 16:58:28 -0700

    Seeded the heck out of our database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404215346) do
+ActiveRecord::Schema.define(version: 20170404235634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170404215346) do
     t.integer  "user_id"
     t.integer  "rating"
     t.float    "cost"
+    t.string   "date"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,45 @@
 require 'csv'
 
 driver_info = File.read(Rails.root.join('db', 'CSV', 'drivers.csv'))
+driver_info = CSV.parse(driver_info, :headers => true)
+
+driver_info.each do |row|
+  d = Driver.new
+  d.name = row['name']
+  d.vin = row['vin']
+  d.is_driving = false
+  d.save
+  puts "#{d.name}, #{d.vin}"
+end
+
+
+
 user_info = File.read(Rails.root.join('db', 'CSV', 'riders.csv'))
+user_info = CSV.parse(user_info, :headers => true)
+
+user_info.each do |row|
+  u = User.new
+  u.name = row['name']
+  u.phone_num = row['phone_num']
+  u.save
+  puts "#{u.name}, #{u.phone_num}"
+end
+
+
 trip_info = File.read(Rails.root.join('db', 'CSV', 'trips.csv'))
+trip_info = CSV.parse(trip_info, :headers => true)
+
+trip_info.each do |row|
+  t = Trip.new
+  t.driver_id = row['driver_id']
+  t.user_id = row['rider_id']
+  t.date = row['date']
+  t.rating = row['rating']
+  t.cost = rand((1.01)..(50.99))
+  t.save
+  puts "#{t.cost} and #{t.date}"
+end
+
+puts "There are #{Driver.all.length} drivers saved out of #{driver_info.length}"
+puts "There are #{User.all.length} users saved out of #{user_info.length}"
+puts "There are #{Trip.all.length} trips saved out of #{trip_info.length}"
