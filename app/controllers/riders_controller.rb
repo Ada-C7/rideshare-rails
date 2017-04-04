c2bb47be7205d3a35f80c7c2a7853a5fa69f408a
sai samant <saisamant@gmail.com>
2017-04-04 16:04:57 -0700

    controller actions added

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -1,4 +1,48 @@
 class RidersController < ApplicationController
   def index
   end
+
+  def new
+    @rider = Rider.new
+  end
+
+  def create
+    @rider = Rider.create rider_params
+
+    unless @rider.id == nil
+      redirect_to riders_path
+    else
+      render "new"
+    end
+
+  end
+
+  def edit
+    @rider = Rider.find params[:id]
+  end
+
+  def update
+    @rider = Rider.find params[:id]
+
+    @rider.name = rider_params[:name]
+    @rider.phone = rider_params[:phone]
+
+    if @rider.save
+      redirect_to rider_path
+    else
+      render "edit"
+    end
+
+  end
+
+  def destroy
+    Rider.destroy(params[:id])
+    redirect to riders_path
+
+  end
+
+  private
+  def rider_params
+    params.require(:rider).permit(:name, :phone)
+  end
 end
