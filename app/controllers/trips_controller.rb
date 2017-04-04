36e1bebb54d4db1d615d4d67be6f910b8862203b
sai samant <saisamant@gmail.com>
2017-04-04 14:29:49 -0700

    added create actions for Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,25 @@ class TripsController < ApplicationController
     @trips = Trip.all
   end
 
-  def create
+  def new
+    @trip = Trip.new
   end
+
+  def create
+    @trip = Trip.create trip_params
+
+    unless @trip.id == nil
+      redirect_to trips_path
+    else
+      render "new"  #need to add validations to trip model
+    end
+  end
+
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:driver_id, :rider_id, :date, :rating, :cost)
+
 end
