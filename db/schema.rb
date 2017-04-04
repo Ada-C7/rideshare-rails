5f71d47023b6e01382f7ffd82bf6a7016c29a450
Ashtn <ashtonehorton@gmail.com>
2017-04-04 14:54:59 -0700

    models created for trip, driver & user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404212646) do
+ActiveRecord::Schema.define(version: 20170404215346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,11 +26,17 @@ ActiveRecord::Schema.define(version: 20170404212646) do
   create_table "trips", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "driver_id"
+    t.integer  "user_id"
+    t.integer  "rating"
+    t.float    "cost"
   end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "phone_num"
   end
 
 end
