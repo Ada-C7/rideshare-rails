23884d96488a0f566e97c30fc16ef31f0c153ac5
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-04 14:58:21 -0700

    added driver_params, index, new, create, and show methods to Driver controller and empty view folders to app

--- app/controllers/driver_controller.rb
+++ app/controllers/driver_controller.rb
@@ -1,14 +1,20 @@
 class DriverController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
+    Driver.create(driver_params)
+    #redirect_to #we can choose path together
   end
 
   def show
+    id = params[:id].to_i
+    @driver = Driver.find(id) 
   end
 
   def edit
@@ -18,6 +24,11 @@ class DriverController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :id)
   end
 
 end
