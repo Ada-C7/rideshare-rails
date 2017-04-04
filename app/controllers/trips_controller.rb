0b7622920cf8676047a7c027744e80cb8e223014
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-04 16:51:46 -0700

    displays array of trips for passenger

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,44 @@
 class TripsController < ApplicationController
-end
+
+  def index
+    @trips = Trip.all
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    Trip.create(trip_params)
+    redirect_to trips_path
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+
+
+    def update
+      trip = Trip.find(params[:id])
+      trip.update_attributes(trip_params)
+      trip.save
+      redirect_to trips_path(trip)
+    end
+
+    def destroy
+      trip = Trip.find(params[:id])
+      trip.destroy
+      redirect_to trips_path
+    end
+
+    private
+    def trip_params
+      return params.require(:trip).permit(:id, :driver_id, :passenger_id, :date, :rating, :price)
+    end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
 resources :passengers, except: [:new]
+resources :trips
 end
