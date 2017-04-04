3eeb49143bb0ba6bfeb3dfce9127f0206ffb5288
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-04 16:12:26 -0700

    added methods into driver controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,44 @@
 class DriversController < ApplicationController
+# View all drivers
+  def index
+    @drivers = Driver.all
+  end
+
+# View one driver
+  def show
+    @driver = Driver.find(params[:id])
+  end
+
+# Create a driver
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    Driver.create(driver_params)
+    redirect_to driver_path
+  end
+
+# Edit a driver
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    driver = Driver.find(params[:id])
+    driver.update(animal_params)
+
+    redirect_to driver_path(driver)
+  end
+
+# Delete a driver
+  def destroy
+    Driver.find(params[:id]).destroy
+    redirect_to driver_path
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
