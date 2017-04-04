942efecc5ec45cebb75ad93ab569e738e07a839f
amb54 <ambarklund@hotmail.com>
2017-04-04 16:52:24 -0700

    For Driver: Added show, new-create, edit-update, and destroy  in the drivers index view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,34 +1,48 @@
 class DriversController < ApplicationController
   def index
 
-    @drivers = Driver.all
+    @drivers = Driver.all.order(:id)
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    Driver.create(driver_params)
+    redirect_to drivers_path
   end
 
   def show
     @driver = Driver.find(params[:id])
   end
 
-  def new
-
-  end
-
-  def create
-
-  end
-
 
   def edit
-
+    @driver = Driver.find(params[:id])
+    session.delete(:return_to)
+    session[:return_to] ||= request.referer
+    @back_url = session[:return_to]
   end
 
   def update
-
+    driver = Driver.find(params[:id])
+    driver.update_attributes(driver_params)
+    driver.save
+    # redirect_to animal_path(animal)
+    redirect_to session[:return_to]
   end
 
   def destroy
-
+    driver = Driver.find(params[:id])
+    driver.destroy
+    redirect_to drivers_path
   end
-
+  #### PRIVATE
+  private
+  def driver_params
+    params.require(:driver).permit(:name, :vin)
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
 
     #DRIVERS
       get 'drivers', to: 'drivers#index', as: 'drivers'
+
+      get 'drivers/new', to: 'drivers#new', as: 'new_driver'
+      post 'drivers', to: 'drivers#create'
+
       get 'drivers/:id', to: 'drivers#show', as: 'driver'
+      get 'drivers/:id/edit', to: 'drivers#edit', as: 'edit_driver'
+
+      patch 'drivers/:id', to: 'drivers#update'
+
+      delete 'drivers/:id', to: 'drivers#destroy'
 
 
     #RIDERS
