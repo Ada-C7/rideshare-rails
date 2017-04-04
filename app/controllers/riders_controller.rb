42ba0796e616db1b9dcdd23bb1c4b35750e347b3
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-04 14:55:32 -0700

    Created create method for riders_controller

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -10,4 +10,18 @@ class RidersController < ApplicationController
   def new
     @rider = Rider.new
   end
+
+  def create
+    rider = Rider.create rider_params
+
+    unless rider.id == nil
+      redirect_to riders_path
+    end
+  end
+
+  private
+
+  def rider_params
+    params.require(:rider).permit(:name, :phone_num)
+  end
 end
