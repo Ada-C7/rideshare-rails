0d7ec8f3a26b8e52c6d2ed35f3af38147227b7f7
EricaJCase <ericajcase@gmail.com>
2017-04-04 14:38:52 -0700

    finished rider and driver index pages, controllers, and rider show page

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,49 @@
 class DriversController < ApplicationController
+
+  def index
+    @drivers = Driver.all
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    driver = Driver.find(params[:id])
+    driver.destroy
+
+    redirect_to drivers_path
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def show
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    driver = Driver.find(params[:id])
+    driver.update_attributes(driver_params)
+    driver.save
+
+    redirect_to drivers_path
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :phone_num)
+  end
+
 end
