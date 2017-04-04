834f6de8516c839ff9466263bfdf69d7be151b5b
Cara Comfort <cara.comfort10@gmail.com>
2017-04-04 16:19:01 -0700

    Seeded Driver, Passenger, and Trip info from given CSV files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+# passengers
+passenger_file = Rails.root.join('lib', 'seeds', 'passengers.csv')
+
+CSV.foreach(passenger_file, headers: true) do |row|
+  Passenger.create(name: row['name'], phone_number: row['phone_num'])
+end
+
+# drivers
+driver_file = Rails.root.join('lib', 'seeds', 'drivers.csv')
+
+CSV.foreach(driver_file, headers: true) do |row|
+  Driver.create(name: row['name'], vin: row['vin'])
+end
+
+# trips
+trip_file = Rails.root.join('lib', 'seeds', 'trips.csv')
+
+CSV.foreach(trip_file, headers: true) do |row|
+  if row['driver_id'] != "0" && row['passenger_id'] != "0"
+    Trip.create(driver_id: row['driver_id'].to_i, passenger_id: row['passenger_id'].to_i,
+      date: row['date'].to_date, rating: row['rating'].to_i)
+  end
+end
