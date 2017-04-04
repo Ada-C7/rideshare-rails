7b7856fb57e584af6bd334ecb6aa3934534d07b2
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-04 12:31:25 -0700

    want csv pulled in seeds.rb not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CSV.read(('../support/passengers.csv').path, :headers => :true).each do |passenger|
+  Passenger.create(passenger)
+end
+
+CSV.read(('../support/drivers.csv').path, :headers => :true).each do |driver|
+  Driver.create(driver)
+end
+
+CSV.read(('../support/trips.csv').path, :headers => :true).each do |trip|
+  Trip.create(trip)
+end
