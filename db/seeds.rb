b72760ccb22a10e47a8ff8196c6334775d05d96d
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-04 16:25:22 -0700

    read from CSV, made passenger index, need to show only a few names so we can make a table with HTML/CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "CSV"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,15 +7,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-passengers = [
-  {
-    name: "Sandi Metz"
-  },
-  {
-    name: "Chantal"
-  }
-]
 
-passengers.each do |passenger|
-  Passenger.create(passenger)
+# passengers = []
+
+CSV.foreach("support/passengers.csv", :headers => true) do |line|
+  p = Passenger.new
+  p.passenger_id = line[0].to_i
+  p.name = line[1].to_s
+  p.phone_num = line[2].to_s
+  p.save
+
+  puts "#{p.name} saved"
+
+  # passengers << Passenger.create(:passenger_id => line[0].to_i, :name => line[1].to_s,
+  # :phone_num => line[2].to_s)
+
 end
+
+# passengers = [
+#   {
+#     name: "Sandi Metz"
+#   },
+#   {
+#     name: "Chantal"
+#   }
+# ]
+#
+# passengers.each do |passenger|
+#   Passenger.create(passenger)
+# end
