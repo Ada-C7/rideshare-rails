17f405e8e3fc7d043bfaec5c5cfb702b9ae7d7de
Leesyruiz <leesyruiz@gmail.com>
2017-04-03 17:00:20 -0700

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'drivers/index'
 
-  get 'riders/index'
+    #DRIVERS
+      get 'drivers/index'
+
+
+    #RIDERS
+      get 'riders/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
