a1f9ccf67cf084a46d957626e96fcf3d2a94ea47
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-04 14:44:06 -0700

    Created first view for passengers! YAY! Add CSV next

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+passengers = [
+  {
+    name: "Sandi Metz"
+  },
+  {
+    name: "Chantal"
+  }
+]
+
+passengers.each do |passenger|
+  Passenger.create(passenger)
+end
