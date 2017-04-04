382b059c4e7bee1c22f49921447e5a612fdb3ff1
Sofia Kim <sofiamhkim@gmail.com>
2017-04-04 14:54:07 -0700

    db migrated and seed a rails db with csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,35 @@ csv_text = File.read(Rails.root.join('lib', 'seeds', 'trips.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
   trip = Trip.new
-  trip.id = row['id']
+  trip.trip_id = row["trip_id"]
+  trip.driver_id = row["driver_id"]
+  trip.passenger_id = row["passenger_id"]
+  trip.date = row["date"]
+  trip.rating = row["rating"]
 
   trip.save
+end
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'passengers.csv'))
+
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  passenger = Passenger.new
+  passenger.passenger_id = row["passenger_id"]
+  passenger.name = row["name"]
+  passenger.phone_num = row["phone_num"]
+
+  passenger.save
+end
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'drivers.csv'))
+
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  driver = Driver.new
+  driver.driver_id = row["driver_id"]
+  driver.name = row["name"]
+  driver.vin = row["vin"]
+
+  driver.save
 end
