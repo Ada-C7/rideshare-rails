7640309fb4e659d8abc4cac049af3f9b0e638a72
Maria McGrew <mcg.maria@gmail.com>
2017-04-05 15:30:41 -0700

    added index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'passengers/index'
-
-  get 'drivers/index'
-
-  get 'trips/index'
+  # get 'passengers/index'
+  #
+  # get 'drivers/index'
+resources :trips, only:[:index]
+  # get 'trips' to: 'trips#index', as: 'trips'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
