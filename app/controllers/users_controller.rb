913001158c8d2167b3421e98d63bded98c7bf6b9
Ashtn <ashtonehorton@gmail.com>
2017-04-04 22:11:56 -0700

    user controller actions initiated minus redirection paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,43 @@
 class UsersController < ApplicationController
 
 def index
+  @users = User.all
 end
 
 def create
+  @user = User.create user_params #params[:user]
+
+  if @user.id != nil
+    redirect_to #users_path
+  else
+    render "new"
+  end
 end
 
 def new
-end 
+  @user = User.new
+end
 
 def edit
+  @user = User.find(params[:id])
 end
 
 def show
+  @user = User.find(params[:id])
 end
 
 def update
+  @user = User.find(params[:id])
+  if @user.udpate(user_params)
+    redirect_to #user_path
+  else
+    render "edit"
+  end
 end
 
 def destroy
+  user.destroy(params[:id])
+  redirect_to #users_path
 end
 
 end
