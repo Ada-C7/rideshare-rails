962f97ea6dd0d814f2bdfd1112a4d203e7a201d3
add2point71dots <alcland@gmail.com>
2017-04-05 16:47:09 -0700

    can now add a passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -10,4 +10,15 @@ class PassengersController < ApplicationController
   def new
     @passenger = Passenger.new
   end
+
+  def create
+    passenger = Passenger.create(passenger_params)
+    redirect_to passengers_path unless passenger.id == nil
+  end
+
+  private
+
+  def passenger_params
+    params.require(:passenger).permit(:name, :phone)
+  end
 end
