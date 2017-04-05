ba7410c5cf4eaf7bb4ccadec289756b9dafe49ba
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-05 15:07:39 -0700

    added show method and html for pass, need to do the same for driver and trips

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,5 +3,9 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
-  
+
+  def show
+    @result_passenger = Passenger.find(params[:id])
+  end
+
 end
