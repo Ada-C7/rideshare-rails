6d4356ee80196746a44744db37d57cdc82e6ecce
Kayla <kubicke.kayla@gmail.com>
2017-04-05 15:11:25 -0700

    Created skeleton methods for drivers_controller.rb.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,28 @@
 class DriversController < ApplicationController
+  # drivers GET    /drivers(.:format)             drivers#index
+  # POST   /drivers(.:format)             drivers#create
+  # new_driver GET    /drivers/new(.:format)         drivers#new
+  # edit_driver GET    /drivers/:id/edit(.:format)    drivers#edit
+  # driver GET    /drivers/:id(.:format)         drivers#show
+  # PATCH  /drivers/:id(.:format)         drivers#update
+  # PUT    /drivers/:id(.:format)         drivers#update
+  # DELETE /drivers/:id(.:format)         drivers#destroy
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
