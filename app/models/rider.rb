a3d144f0e92ba8e40732ac6af47354a9c991a5ff
natalia-ku <bilberryfm@mail.ru>
2017-04-04 19:00:16 -0700

    added validation for Rider

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,6 +1,11 @@
 class Rider < ApplicationRecord
   has_many :trips
 
+  validates :name, presence: true
+  validates :name, format: {with: /[a-zA-Z]+/}
+
+  validates :phone_num, presence: true
+
   def total_amount
     total = 0
     self.trips.each do |trip|
