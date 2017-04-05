66d099368f580ed20700cf8156073f7d7e58efad
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-05 15:24:32 -0700

    adds index and show for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,5 +1,6 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def create
@@ -12,6 +13,7 @@ class DriversController < ApplicationController
   end
 
   def show
+    @driver = Driver.find(params[:id])
   end
 
   def update
