1c001e150aed1d6adff4a0451dd5c5064cbe675c
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-04 23:04:01 -0700

    added total cost calculation logic to model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,11 @@
 class Driver < ApplicationRecord
     has_many :trips
+
+    def total_amount
+      total = 0
+      self.trips.each do |trip|
+        total += trip.cost
+      end
+      return total
+    end
 end
