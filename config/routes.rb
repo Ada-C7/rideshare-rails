8fe1af4cd88082e6567214d6c428d567fcce363f
Anacelia Gillette <acgillette426@gmail.com>
2017-04-05 16:13:20 -0700

    Dryin out dat code:

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,8 @@
 Rails.application.routes.draw do
-  get 'driver/index'
-
-  get 'driver/new'
-
-  get 'driver/update'
-
-  get 'driver/delete'
-
-  get 'driver/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/drivers/:id/edit", to: "drivers#edit", as: 'edit_driver'
-  patch "/drivers/:id", to: "drivers#update"
 
-  delete "/drivers/:id", to: "drivers#destroy", as: "delete_driver"
+  resources :drivers
 
-  resources :drivers, except: [:edit, :update, :delete]
-
-
-  resources :users
-  resources :trips
 
 end
