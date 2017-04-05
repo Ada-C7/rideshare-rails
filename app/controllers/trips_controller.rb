ca24cd886249f4b3120eab8f45cbea526b27fdde
Haby Randall <sow.haby@gmail.com>
2017-04-05 16:32:40 -0700

    Added update method to trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,4 +12,25 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def update
+    trip = Trip.find(params[:id])
+    trip.driver_id = trip_params[:driver_id]
+    trip.rider_id = trip_params[:rider_id]
+    trip.date = trip_params[:date]
+    trip.rating = trip_params[:rating]
+
+    if trip.save
+      redirect_to trip_path(trip.id)
+    end
+
+  end
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:driver_id, :rider_id, :date, :rating)
+  end
+
+
 end
