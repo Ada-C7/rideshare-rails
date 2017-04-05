adade88304558fd1d4c1b26ed238fecc643e7c3c
Anacelia Gillette <acgillette426@gmail.com>
2017-04-04 19:35:12 -0700

    Had to fix some route issues

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   get 'driver/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/drivers/:id/edit", to: "drivers#edit", as: 'edit_task'
+  patch "/drivers/:id", to: "drivers#update"
 
-  resources :drivers
+  resources :drivers, except: [:edit, :update]
+
+
   resources :users
   resources :trips
 
