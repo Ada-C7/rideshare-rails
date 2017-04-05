7e982908eb095cbec3ebb9649e22613248217bca
Bo Trethewey <botrethewey@gmail.com>
2017-04-05 15:43:22 -0700

    added controller actions for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,10 +1,47 @@
 class DriversController < ApplicationController
   def index
-  end
-
-  def new
+    @drivers = Driver.all
   end
 
   def show
+    @result_driver = Driver.find(params[:id])
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+
+    if @driver.update(driver_params)
+      redirect_to driver_path
+    else
+      render "edit"
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.create driver_params
+    if @driver.id != nil
+      redirect_to drivers_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    Driver.destroy(params[:id])
+    redirect_to drivers_path
+  end
+
+  private
+
+  def driver_params
+    params.require(:driver).permit(:name, :vin, :available)
   end
 end
