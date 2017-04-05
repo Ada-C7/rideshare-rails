0b268bf4389146f80038a486a57e7039ff9cdd5f
Sahana Murthy <s.murthy730.com>
2017-04-05 15:27:50 -0700

    Refactored total_amount_charged feature to be in the passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,13 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_amount_charged
+    total_amount_charged = 0
+    array = self.trips
+    array.each do |trip|
+      total_amount_charged += trip.price
+    end
+    total_amount_charged
+  end
+
 end
