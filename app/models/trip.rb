104424070f0ea479263a3241d437b26b6c7ee9a5
Ann Dai <gofarann@gmail.com>
2017-04-05 15:08:21 -0700

    created validators for trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,15 @@
 class Trip < ApplicationRecord
+  validates :driver_id, presence: true,
+                        uniqueness: true,
+                        numericality: { only_integer: true }
+
+
+  validates :rider_id, presence: true
+                       uniqueness: true,
+                       numericality: { only_integer: true }
+
+  validates :date, presence: true
+
+  validates :cost, presence: true
+
 end
