988e556a0e9b01b82d3ad804ef86295e4304edff
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-05 15:56:08 -0700

    generated passenger controller, wrote RESTful passenger methods, start of index, added passenger routes

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,45 @@
+class PassengersController < ApplicationController
+
+  def index
+    @passengers = Passenger.all
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    Passenger.create(passenger_params)
+
+    redirect_to passengers_path
+  end
+
+  def show
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def edit
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    passenger.update_attributes(passenger_params)
+    passenger.save
+
+    redirect_to passenger_path(passenger)
+  end
+
+  def destroy
+    passenger = Passenger.find(params[:id])
+    passenger.destroy
+
+    redirect_to passengers_path
+  end
+
+  private
+    def passenger_params
+      return params.require(:passenger).permit(:name, :phone_num)
+    end
+
+end
