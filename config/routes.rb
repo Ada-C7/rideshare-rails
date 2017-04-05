4866bc59c50c5044e6964b1f2305d4bc4f25314d
Maria McGrew <mcg.maria@gmail.com>
2017-04-05 15:48:29 -0700

    added welcome controller, welcome route, made a welcome index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#index', as: 'welcome'
+  # get 'welcome/index'
+
   # get 'passengers/index'
   #
   # get 'drivers/index'
-resources :trips, only:[:index]
+resources :trips, only:[:index, :show]
+resources :welcome, only:[:index, :show]
   # get 'trips' to: 'trips#index', as: 'trips'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
