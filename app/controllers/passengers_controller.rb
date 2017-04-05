a79cc0869bb71ca769d0b2a5a1ae9f6485353682
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-05 15:44:58 -0700

    created show controller action and view

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,9 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
+
+  def show
+    @result_passenger = Passenger.find(params[:id])
+  end
+
 end
