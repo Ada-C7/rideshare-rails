c7ded859b992dd891b1dbca567b5ae8bbf146bf1
Janice Lichtman <jalichtman@gmail.com>
2017-04-04 17:00:07 -0700

    used resources for Restful routing for passengers, and enabled functionality to add a new passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,8 +5,25 @@ class PassengersController < ApplicationController
   end
 
 
+  def new
+     @passenger = Passenger.new
+  end
+
+  def create
+    Passenger.create(passenger_params)
+    redirect_to passengers_path
+  end
+
+
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+
+  private
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_number)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get 'passengers', to:'passengers#index', as:'passengers'
-
-
-get 'passengers/:id', to:'passengers#show', as: 'passenger'
-
+resources :passengers
 
 end
