0fc923db5178a41df3eb748ebb725a26013b3500
Sahana Murthy <s.murthy730.com>
2017-04-05 16:02:56 -0700

    Created edit passenger feature

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,10 +5,13 @@ class PassengersController < ApplicationController
     @passengers = Passenger.all
   end
 
-
   def create
     Passenger.create(passenger_params)
     redirect_to passengers_path
+  end
+
+  def edit
+    @passenger = Passenger.find(params[:id])
   end
 
   def show
