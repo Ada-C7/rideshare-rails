17f2cabf81236c2ef2014629678850fe3e5b6e2a
amb54 <ambarklund@hotmail.com>
2017-04-05 15:45:30 -0700

    Calculate average rating for the driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,16 @@ class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, presence: true, length: {is: 17}
   has_many :trips
+
+
+
+  def average
+    array_of_ratings = trips.all.collect {|t| t.rating}
+    if array_of_ratings != []
+      return array_of_ratings.sum.to_f/array_of_ratings.length
+    else
+      return Float::NAN
+    end
+  end
+  
 end
