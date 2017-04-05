89ef2f13b6ea4f5864e03fd31232c7c816836fd1
Haby Randall <sow.haby@gmail.com>
2017-04-04 17:02:17 -0700

    Merging conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
   root 'drivers#index'
 
   resources :drivers
-=======
   resources :riders
->>>>>>> eca214477b6b54e748476ee8f8d33f35d59328a2
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20170404212650) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
