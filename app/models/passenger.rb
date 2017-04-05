ba181f03f17401ccc7499a7d3f33d13b53146a49
Sahana Murthy <s.murthy730.com>
2017-04-05 15:37:06 -0700

    Refactored total amount charged method for Passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,8 +3,7 @@ class Passenger < ApplicationRecord
 
   def total_amount_charged
     total_amount_charged = 0
-    array = self.trips
-    array.each do |trip|
+    self.trips.each do |trip|
       total_amount_charged += trip.price
     end
     total_amount_charged
