1cb48001f2e400369da5203c4a67d41895e81d06
laura <lmelgarejos@unal.edu.co>
2017-04-05 15:31:21 -0700

    created trip's controller

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,48 @@
+class TripsController < ApplicationController
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+
+    if @trip.save
+      redirect_to trips_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    @trip.update_attributes(trip_params)
+
+    if @trip.save
+      redirect_to trips_path(@trip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    trip = Trip.find(params[:id])
+    trip.destroy
+
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-resources :riders, :drivers
+resources :riders, :drivers, :trips
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # get 'riders', to: 'riders#index', as: 'riders'
 # get 'riders/new', to: 'riders#new', as: 'new_rider'
 # post 'riders', to: 'riders#create'
