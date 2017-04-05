6d7e7286bb4cb06b49f845379c8f154863ad3db2
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-05 16:54:00 -0700

    adjusted buttons on edit form; created update controller method

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -10,4 +10,22 @@ class PassengersController < ApplicationController
   def edit
     @result_passenger = Passenger.find(params[:id])
   end
+
+  def update
+    @passenger = Passenger.find(params[:id])
+
+    @passenger.name = passenger_params[:name]
+    @passenger.phone_num = passenger_params[:phone_num]
+
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    end
+  end
+
+  private
+
+  def passenger_params
+    params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
