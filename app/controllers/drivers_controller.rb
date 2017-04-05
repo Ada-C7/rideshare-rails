2e50abff1105d5c63277f450bbdca14d43590e9d
Cara Comfort <cara.comfort10@gmail.com>
2017-04-05 15:42:40 -0700

    Added new/credit method in drivers controller and new view for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -4,7 +4,23 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.create(driver_params)
+    redirect_to drivers_path
+  end
+
   def show
     @driver = Driver.find(params[:id])
   end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+
 end
