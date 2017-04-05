13e55a34a0955f1411aba527fd1a7dd063c12dac
Theresa Manney <theresa.m.17@gmail.com>
2017-04-04 17:34:17 -0700

    seed file can read from passenger csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'passengers.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  pass = Passenger.new
+  pass.passenger_id = row['passenger_id']
+  pass.name = row['name']
+  pass.phone_num = row['phone_num']
+  pass.save
+  puts "#{pass.passenger_id}, #{pass.name}, #{pass.phone_num} saved!"
+end
+
+puts "There are now #{Passenger.count} rows in the passengers table"
