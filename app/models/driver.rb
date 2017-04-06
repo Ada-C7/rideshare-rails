4497278a1aeb7136a03e967e3120671c6ba23157
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-06 06:55:14 -0700

    added validations to trip and driver models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,15 @@
 class Driver < ApplicationRecord
     has_many :trips
 
+      validates :name, presence: true
+      validates :name, format: {with: /[a-zA-Z]+/}
+
+      validates :vin, presence: true
+      validates :vin, format: {with: /[1-9a-zA-Z]+/}
+
+      validates :driver_id, presence: true
+      validates :driver_id, numericality: {greater_than: 0}
+      
     def total_amount
       total = 0
       self.trips.each do |trip|
