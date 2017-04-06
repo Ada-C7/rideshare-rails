bbfa63fcb15064d06357036ab071bac9de264b2b
Andrea Valliere <amvalliere@gmail.com>
2017-04-05 22:44:12 -0700

    Seeded drivers.csv into rideshare app. Parsed data, created loop, and made each entry a database object

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'drivers.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  d = Driver.new
+  d.driver_id = row['driver_id']
+  d.name = row['name']
+  d.vin = row['vin']
+  d.save
+  puts "Driver #{d.name} saved"
+end
+
+puts "There are #{Driver.count} drivers"
