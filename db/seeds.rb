2afbabbd8526ce9bd45a681e6d5234150282a172
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-05 20:59:25 -0700

    updated seeds file to read in trips.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,19 @@ csv.each do |row|
 end
 
 puts "There are now #{Passenger.count} rows in the passengers table"
+
+
+csv_text2 = File.read(Rails.root.join('lib', 'seeds', 'trips.csv'))
+csv2 = CSV.parse(csv_text2, :headers => true, :encoding => 'ISO-8859-1')
+
+csv2.each do |row|
+  trip = Trip.new
+  trip.driver_id = row['driver_id']
+  trip.passenger_id = row['passenger_id']
+  trip.date = row['date']
+  trip.rating = row['rating']
+  trip.save
+  puts "#{trip.driver_id}, #{trip.passenger_id}, #{trip.date}, #{trip.rating} saved!"
+end
+
+puts "There are now #{Trip.count} rows in the passengers table"
