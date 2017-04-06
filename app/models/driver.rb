526ef2ff45689e8533be1c53b6597893a86cbafd
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-06 16:31:06 -0700

    added driver model method to calcualte total earnings - driver show is now showing this total

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,6 +3,12 @@ class Driver < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }
   validates :vin, presence: true, uniqueness: true, length: { is: 17 }
 
+  def get_trips
+    trips = Trip.all
+    trips_of_driver = trips.map { |trip| trip if trip.driver_id == id}.compact
+    trips_of_driver.empty? ? nil : trips_of_driver
+  end
+
   def calculate_average_rating()
     trips = get_trips
     return nil if trips.nil?
@@ -11,9 +17,13 @@ class Driver < ApplicationRecord
     average.round(2)
   end
 
-  def get_trips
-    trips = Trip.all
-    trips_of_driver = trips.map { |trip| trip if trip.driver_id == id}.compact
-    trips_of_driver.empty? ? nil : trips_of_driver
+  def calculate_earnings
+    trips = get_trips
+    return nil if trips.nil?
+    total_cost_in_cents = trips.map { |trip| trip.cost_in_cents }.sum
+    total_cost_in_dollars = total_cost_in_cents / 100
+    drivers_earnings = total_cost_in_dollars * 0.85
+    drivers_earnings.round(2)
   end
+
 end
