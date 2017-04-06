2d9432911e71c81b8c16f3be102cf1deaec4701b
sai samant <saisamant@gmail.com>
2017-04-05 17:00:20 -0700

    added business logic to Rider model to calculate total_cost. boss.

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -3,4 +3,15 @@ class Rider < ApplicationRecord
   validates :name, presence: true
 
   validates :phone, presence: true
+
+  def total_cost id
+    rider = Rider.find id
+    total_cost = 0
+    rider.trips.each do  |trip|
+      total_cost += trip.cost
+    end
+    return total_cost
+  end
+
+
 end
