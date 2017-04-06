4a2b9b1642b9fd7b11bbc47f257efaedf13184c1
Brenna <bcmdarroch@gmail.com>
2017-04-05 17:25:21 -0700

    saved layout diagrams, created _form, edit working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,31 @@ require 'csv'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# passengers_array = []
-# CSV.read("support/passengers.csv", headers: true).map do |line|
-#   passenger = {name: line[1], phone: line[2]}
-#   passengers_array << passenger
-# end
-#
-# passengers_array.each do |passenger|
-#   newp = Passenger.create!(passenger)
-#   if !newp.id
-#     puts "couldn't create passenger #{passenger.name}"
-#   end
-# end
-#
-# drivers_array = []
-# CSV.read("support/drivers.csv", headers: true).map do |line|
-#   driver = {name: line[1], vin: line[2]}
-#   drivers_array << driver
-# end
-#
-# drivers_array.each do |driver|
-#   newd = Driver.create!(driver)
-#   if !newd.id
-#     puts "couldn't create driver #{driver.name}"
-#   end
-# end
+passengers_array = []
+CSV.read("support/passengers.csv", headers: true).map do |line|
+  passenger = {name: line[1], phone: line[2]}
+  passengers_array << passenger
+end
+
+passengers_array.each do |passenger|
+  newp = Passenger.create!(passenger)
+  if !newp.id
+    puts "couldn't create passenger #{passenger.name}"
+  end
+end
+
+drivers_array = []
+CSV.read("support/drivers.csv", headers: true).map do |line|
+  driver = {name: line[1], vin: line[2]}
+  drivers_array << driver
+end
+
+drivers_array.each do |driver|
+  newd = Driver.create!(driver)
+  if !newd.id
+    puts "couldn't create driver #{driver.name}"
+  end
+end
 
 trips_array = []
 CSV.read("support/trips.csv", headers: true).map do |line|
