ba1a1dcb34ebc1c9fc6a0977b03ac52058122b30
Bo Trethewey <botrethewey@gmail.com>
2017-04-05 17:03:34 -0700

    WIP: added trips views and controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,47 @@
 class TripsController < ApplicationController
   def index
-  end
-
-  def new
+    @trips = Trip.all
   end
 
   def show
+    @result_trip = Trip.find(params[:id])
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+
+    if @trip.update(trip_params)
+      redirect_to trip_path
+    else
+      render "edit"
+    end
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create trip_params
+    if @trip.id != nil
+      redirect_to trips_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    Trip.destroy(params[:id])
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:cost, :rating, :driver_id, :passenger_id)
   end
 end
