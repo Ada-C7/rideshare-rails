e5c73d009104f9261d10225e867927727baa3fca
sai samant <saisamant@gmail.com>
2017-04-05 21:05:11 -0700

    list of trips for driver in show view; average rating method in Driver model; driver earnings WIP

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,9 +1,22 @@
 class Driver < ApplicationRecord
   has_many :trips
-  
+
   validates :name, presence: true
 
   validates :vin, presence: true,
                   length: { is: 17 }, allow_blank: false
+
+  def average_rating id
+    driver = Driver.find id
+    trip_count = 0
+    total_rating = 0.0
+    driver.trips.each do |trip|
+      trip_count += 1
+      total_rating += trip.rating
+    end
+    return (total_rating/trip_count).round(2)
+  end
+
+
 
 end
