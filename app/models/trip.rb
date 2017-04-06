485e932fae2eab857579f669f2b3e4d9caa73658
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-06 14:05:08 -0700

    added errors to all, need to fix an error arising when creating new trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,10 +3,8 @@ class Trip < ApplicationRecord
 
   belongs_to :passenger
 
-  validates :id, presence: true, uniqueness: true
   validates :driver_id, presence: true
   validates :passenger_id, presence: true
   validates :date, presence: true
-  validates :rating, presence: true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
-  # inclusion: { in: (1..5) }
+  validates :rating, presence: true, inclusion: { in: (1..5) }
 end
