ad9e8ba48b2f93e3e2d036e21e44f6163e964658
Haby Randall <sow.haby@gmail.com>
2017-04-06 15:50:53 -0700

    Working on riders select rating function

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -3,4 +3,11 @@ class Rider < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, format: {with: /\A[ a-zA-Z]+\z/, message: "only allows letters" }
   validates :phone_num, presence: true, uniqueness: true
+
+
+  def total_cost
+    cost_array = trips.map(&:cost)
+    return (cost_array.sum).round(2)
+  end
+
 end
