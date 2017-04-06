8d7567b2e66ada5547d0a83f6bd20c19099861c3
Haby Randall <sow.haby@gmail.com>
2017-04-06 00:43:36 -0700

    Added validations to driver.rb

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,7 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true, uniqueness: true, format: {with: /\A[ a-zA-Z]+\z/, message: "only allows letters" }
+  validates :vin, presence: true, uniqueness: true, length: { is: 17 }
+  validates :availability, inclusion: { in: [true, false] }, allow_nil: true
 end
