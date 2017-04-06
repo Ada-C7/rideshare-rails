cd9e94883ece2412db3e95e797473cc3901e6f69
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-05 17:22:12 -0700

    added method in driver model to calculate rating, added html show show page to display rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -8,4 +8,12 @@ class Driver < ApplicationRecord
     end
     total_amount_earned -= (total_amount_earned*0.15)
   end
+
+  def rating
+    added_ratings = 0
+    self.trips.each do |trip|
+      added_ratings += trip.rating
+    end
+    added_ratings / self.trips.length
+  end
 end
