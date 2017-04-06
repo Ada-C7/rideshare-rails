d4f0895fa1bd13b1e6178275ab4c07d742586183
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-06 13:54:59 -0700

    added error message to driver form, showing up in a strange format

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,8 +1,6 @@
 class Driver < ApplicationRecord
   has_many :trips
-  
-  validates :id, presence: true, uniqueness: true
+
   validates :name, presence: true
-  validates :vin, presence: true, uniqueness: true, length: (17)
-  validates :avg_rating, presence: true
+  validates :vin, presence: true, uniqueness: true, length: { is: 17 }
 end
