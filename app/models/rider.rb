24d7d03c6085f68760dddc0c1c4c398e22933c34
Haby Randall <sow.haby@gmail.com>
2017-04-06 00:59:17 -0700

    Adding validations to rider and trip.rb

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,3 +1,6 @@
 class Rider < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true, uniqueness: true, format: {with: /\A[ a-zA-Z]+\z/, message: "only allows letters" }
+  validates :phone_num, presence: true, uniqueness: true
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
 class Trip < ApplicationRecord
   belongs_to :rider
   belongs_to :driver
+
+  # will need to add error messages in views, and possibly more validations 
+
+  validates :driver_id, presence: true
+  validates :rider_id, presence: true
+  validates :date, presence: true
+  validates :rating, presence: true
 end
