75ec415256a68d9266a9f848acd815de5508ab00
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-05 20:32:45 -0700

    actually created Trip model and columns for database

---
+++ db/migrate/20170406030845_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trips do |t|
+      t.integer :driver_id
+      t.integer :passenger_id
+      t.date :date
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405230833) do
+ActiveRecord::Schema.define(version: 20170406030845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drivers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "passengers", force: :cascade do |t|
     t.string   "name"
     t.string   "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer  "driver_id"
+    t.integer  "passenger_id"
+    t.date     "date"
+    t.integer  "rating"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
