fb07d8714593d6aa8a39c3b425acebe1a2311456
add2point71dots <alcland@gmail.com>
2017-04-06 23:12:03 -0700

    can edit a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,4 +10,24 @@ class TripsController < ApplicationController
   def new
     @trip = Trip.new
   end
+
+  def create
+    trip = Trip.create(trip_params)
+    redirect_to trips_path unless trip.id == nil
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    trip = Trip.find(params[:id])
+    if trip.update(trip_params)
+      redirect_to trip_path
+    end
+  end
+
+  def trip_params
+    params.require(:trip).permit(:date, :cost, :rating, :driver_id, :passenger_id)
+  end
 end
