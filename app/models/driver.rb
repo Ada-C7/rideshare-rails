4ae2eeefd5a2fd6ad5cbd4f470a58ef20cef6a01
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-07 12:06:11 -0700

    handles nil case for driver average rating and total earnings

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -6,12 +6,24 @@ class Driver < ApplicationRecord
 
   validates :vin, presence: true
 
+  def ratings
+    self.trips.map { |trip| trip.rating }.compact
+  end
+
   def average_rating
-    total_ratings = self.trips.map {|trip| trip.rating}.reduce(:+).to_f
-    return (total_ratings/self.trips.count).round(1)
+    if self.ratings.empty?
+      "n/a"
+    else
+      total_ratings = self.ratings.reduce(:+).to_f
+      return (total_ratings/self.trips.count).round(1)
+    end
   end
 
   def total_earnings
-    self.trips.map {|trip| trip.fare}.reduce(:+).round(2)
+    if self.trips.empty?
+      "n/a"
+    else
+      self.trips.map { |trip| trip.fare }.reduce(:+).round(2)
+    end
   end
 end
