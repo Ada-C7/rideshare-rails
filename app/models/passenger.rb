fa252298ca49b355eb71956cd99fb5bc59cb384e
Kayla <kubicke.kayla@gmail.com>
2017-04-07 13:56:55 -0700

    Prevented passenger from creating a new trip without rating all trips.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -7,7 +7,7 @@ class Passenger < ApplicationRecord
   def total_cost
 
     trips_array = trips
-    
+
     cost_array = []
 
     trips_array.each do |trip|
@@ -16,6 +16,16 @@ class Passenger < ApplicationRecord
 
     cost = cost_array.sum
 
+  end
+
+  def can_make_new_trip
+    trips.each do |trip|
+      rating = trip.rating
+      if rating == nil
+        return false
+      end
+    end
+    return true
   end
 
 end
