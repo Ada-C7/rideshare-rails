79822b8c4b45bc5809868ed6f663240cb1383781
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-07 14:26:02 -0700

    added index method to welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "welcome#index", as:'welcome'
+  resources :welcome
   resources :drivers
   resources :riders do
     resources :trips , only: [:new, :create]
