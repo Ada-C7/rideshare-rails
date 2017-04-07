d3b7d4cafa5741086e124ee8877ef6364d1a1288
EricaJCase <ericajcase@gmail.com>
2017-04-07 11:30:22 -0700

    all csv files added to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ require 'csv'
 
 rider_text = File.read(Rails.root.join('lib','seeds','passengers.csv'))
 
-csv = CSV.parse(rider_text, :headers => true, :encoding => 'ISO-8859-1')
+rider_csv = CSV.parse(rider_text, :headers => true, :encoding => 'ISO-8859-1')
 
-csv.each do |row|
+rider_csv.each do |row|
   r = Rider.new
   r.id = row['rider_id']
   r.name = row['name']
@@ -15,3 +15,37 @@ csv.each do |row|
 end
 
 puts "There are now #{Rider.count} rows in the table."
+#########################################################
+driver_text = File.read(Rails.root.join('lib','seeds','drivers.csv'))
+
+driver_csv = CSV.parse(driver_text, :headers => true, :encoding => 'ISO-8859-1')
+
+driver_csv.each do |row|
+  d = Driver.new
+  d.id = row['driver_id']
+  d.name = row['name']
+  d.vin = row['vin']
+  d.save
+
+  puts "Added: #{d.id} #{d.name}  #{d.vin} !"
+end
+
+puts "There are now #{Driver.count} rows in the table."
+########################################################
+trip_text = File.read(Rails.root.join('lib','seeds','trips.csv'))
+
+trip_csv = CSV.parse(trip_text, :headers => true, :encoding => 'ISO-8859-1')
+
+trip_csv.each do |row|
+  d = Trip.new
+  d.id = row['trip_id']
+  d.driver_id = row['driver_id']
+  d.rider_id = row['rider_id']
+  d.date = row['date']
+  d.rating = row['rating']
+  d.save
+
+  puts "Added: #{d.id} #{d.driver_id}  #{d.date} !"
+end
+
+puts "There are now #{Trip.count} rows in the table."
