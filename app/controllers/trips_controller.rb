db6a69cc933a41cd8e7b66cdb69c29aa38fee736
Anacelia Gillette <acgillette426@gmail.com>
2017-04-07 14:23:06 -0700

    I don't even know

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,16 +4,7 @@ class TripsController < ApplicationController
   end
 
   def create
-    #raise
-    #Find random driver and assign it to user, as well as start with a rating of 0
-    #user_id = user making new trip
-    #cost functionality
-    #Date.today for date
-    #Trip will not be saved if last Trip object has rating of 0
 
-    # @trip = Trip.create(trip_params)
-    ##param is empty , how to
-    #user = User.find(find_user)
     last_trip = find_user.trips.last
 
     trip = Trip.new
@@ -23,8 +14,6 @@ class TripsController < ApplicationController
     trip[:driver_id] = Driver.find_driver
 
     if last_trip.rating == 0
-      # redirect_to edit_rating_path(last_trip.id)
-      # redirect_to last_trip_path
       redirect_to edit_rating_path(last_trip.id)
     elsif trip.save
       redirect_to edit_rating_path(trip.id)
