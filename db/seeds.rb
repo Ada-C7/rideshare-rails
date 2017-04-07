128659d306dd3aa6fb782a8660c53e9bf885b453
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-07 10:58:33 -0700

    Removed trip id in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,6 @@ end
 
 CSV.foreach("support/trips.csv", :headers => true) do |line|
   t = Trip.new
-  t.id = line[0].to_i
   t.driver_id = line[1].to_i
   t.passenger_id = line[2].to_i
   t.date = line[3].to_s
