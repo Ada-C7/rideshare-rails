b686462ddca6178352e7df6d376353eeb710bd4d
Jackie Watanabe <sushiigirl@gmail.com>
2017-04-07 14:27:28 -0700

    added all views pages and controller actions for Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,49 @@ class TripsController < ApplicationController
     @trips = Trip.all
   end
 
+  def show
+    @result_trip = Trip.find(params[:id])
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+
+    @trip.driver_id = trip_params[:driver_id]
+    @trip.passenger_id = trip_params[:passenger_id]
+    @trip.date = trip_params[:date]
+    @trip.rating = trip_params[:rating]
+
+    if @trip.save
+      redirect_to trip_path(@trip.id)
+    end
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create trip_params
+
+    redirect_to trips_path
+  end
+
+  def destroy
+    Trip.destroy(params[:id])
+
+    redirect_to trips_path
+  end
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:driver_id, :passenger_id, :date, :rating)
+  end
   # def find_passenger_trips
   #
   # end
