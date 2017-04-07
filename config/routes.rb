3033b2fa9b1b9fe57e55a5283863634b35dfc629
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-07 14:14:54 -0700

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+root "welcome#index", as:'welcome'
   resources :drivers
   resources :riders do
     resources :trips , only: [:new, :create]
