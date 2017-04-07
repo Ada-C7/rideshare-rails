32e850090465a3ead9d707b97725f3662f4ce5ed
Regan Huff <regan.huff@gmail.com>
2017-04-07 11:22:02 -0700

    Add average rating and total earnings to Driver views

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,4 +5,13 @@ class Driver < ApplicationRecord
   validates :name, uniqueness:true
 
   validates :vin, presence: true
+
+  def average_rating
+    total_ratings = self.trips.map {|trip| trip.rating}.reduce(:+).to_f
+    return (total_ratings/self.trips.count).round(1)
+  end
+
+  def total_earnings
+    self.trips.map {|trip| trip.fare}.reduce(:+).round(2)
+  end
 end
