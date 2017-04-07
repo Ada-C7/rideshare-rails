ca64cfb0bc41a91d88bdefdfd1407a64aba7fd5d
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-07 13:59:22 -0700

    fixed shows

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,12 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true, uniqueness: true
+
+  scope :active, -> { where(active: true) }
+  scope :availavle, -> { active }
+
+  def average_rating
+    return 1
+  end
+
 end
