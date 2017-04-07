8d03cb72c95998f19918bb8b8da79f669ad69b44
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-07 14:16:13 -0700

    added loop to driver model, still not displaying avg rating on driver list properly

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,11 +1,20 @@
 class Driver < ApplicationRecord
-  has_many :trips
+  has_many :trips #this gives our driver a trips method that returns a collection of all the trips
 
   validates :name, presence: true
   validates :vin, presence: true, uniqueness: true, length: { is: 17 }
 
+
+
+
   def find_avg_rating
-    @trips = Trip.where(driver_id: @id) # returns an array of hashes
-    return @avg_rating = "%.2f" % (@trips.sum { | trip | trip.rating }/@trips.length.to_f)
+    trips = self.trips
+    total = 0
+
+    trips.each do | trip |
+      # @avg_rating = "%.2f" % (@trips.sum { | trip | trip.rating }/@trips.length.to_f)
+      total += trip.rating.to_f
+    end
+    return total/trips.length
   end
 end
