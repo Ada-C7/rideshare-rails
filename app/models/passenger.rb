267321d5cb18e4a658bc35e59f241896f281671d
add2point71dots <alcland@gmail.com>
2017-04-07 11:49:34 -0700

    changed error messages for passenger validation

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,4 @@
 class Passenger < ApplicationRecord
-  validates :name, presence: true
-  validates :phone, presence: true
+  validates :name, presence: { message: "Please provide the passenger's name." }
+  validates :phone, presence: { message: "Please enter the passenger's phone number." }
 end
