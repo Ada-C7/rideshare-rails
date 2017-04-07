10b51d68ae3f0e9d6435a70f376c7bd3ff05be7b
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-07 10:35:11 -0700

    commiting final version of drivers features - showing list of trips with links to the trip

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,23 +3,24 @@ class Driver < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }
   validates :vin, presence: true, uniqueness: true, length: { is: 17 }
 
-  def get_trips
-    trips = Trip.all
-    trips_of_driver = trips.map { |trip| trip if trip.driver_id == id}.compact
-    trips_of_driver.empty? ? nil : trips_of_driver
-  end
+  # dont need this method - has_many builds this method for me :)
+  # def get_trips
+  #   trips = Trip.all
+  #   trips_of_driver = trips.map { |trip| trip if trip.driver_id == id}.compact
+  #   trips_of_driver.empty? ? nil : trips_of_driver
+  # end
 
   def calculate_average_rating()
-    trips = get_trips
-    return nil if trips.nil?
+    # could possible return "NA" instead of nil - mostly cause you aren't including
+    # any testing with this application
+    return nil if trips.empty?
     ratings = trips.map { |trip| trip.rating }
     average = (ratings.sum.to_f) / (ratings.length)
     average.round(2)
   end
 
   def calculate_earnings
-    trips = get_trips
-    return nil if trips.nil?
+    return nil if trips.empty?
     total_cost_in_cents = trips.map { |trip| trip.cost_in_cents }.sum
     total_cost_in_dollars = total_cost_in_cents / 100
     drivers_earnings = total_cost_in_dollars * 0.85
