950e3d17e953197056bf686319eefbf227582a48
Olivia <ssecretsharer@gmail.com>
2017-04-07 15:54:22 -0700

    added numericality validations

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,5 +2,5 @@ class Driver < ApplicationRecord
   has_many :trips
   has_many :passengers
   validates :name, presence: true, uniqueness: true
-  validates :vin, presence: true, uniqueness: true
+  validates :vin, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,11 +2,9 @@ class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
   validates :driver_id, presence: true
-  validates :passenger_id, presence: true
+  validates :passenger_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
 
-#not sure the syntax of the [foreign_key..] thing...looking up
-#e.g., add_foreign_key :drivers, :trips
 
 # randomizing a drvier for a new trip? This link may e helpful - http://stackoverflow.com/questions/9697133/how-to-randomize-array-element-positions-of-an-activerecordrelation
 # a = drivers.all.shuffle
