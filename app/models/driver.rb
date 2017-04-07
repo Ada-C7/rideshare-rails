3d27fd3e6c61c9ceef554e2eea71ec1b734db6a3
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-07 13:38:10 -0700

    added avg rating math to model, trying to get it to display on driver list

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,9 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true, uniqueness: true, length: { is: 17 }
+
+  def find_avg_rating
+    @trips = Trip.where(driver_id: @id) # returns an array of hashes
+    return @avg_rating = "%.2f" % (@trips.sum { | trip | trip.rating }/@trips.length.to_f)
+  end
 end
