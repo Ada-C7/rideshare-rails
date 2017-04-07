57021a4fa4c673889d24c5cc389918de3ea677c9
janicewilson <janice.wilson@mac.com>
2017-04-06 22:44:55 -0700

    Played with validations.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,16 @@
 class Driver < ApplicationRecord
+     has_many :drivers
+     #
+     validates :name,
+          presence:
+          { message: "Please provide the driver's name." }
+
+     validates :vin,
+          presence: { message: "Please enter the driver's VIN." },
+          uniqueness: { message:
+               "%{value} is associated with another driver. Do try once more."
+          }
+          
+     validates :status,
+          presence: { message: "Please select the driver's status." }
 end
