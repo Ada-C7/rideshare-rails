9718353273505856d7338504c0137f08cbc68286
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-07 14:35:15 -0700

    Added method to passenger to calculate total cost of all trips

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,19 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true, uniqueness: true
+
+  def trips_cost
+    trips = self.trips
+    total = 0
+
+    if trips.length == 0
+      return 0
+    end
+
+    trips.each do | trip |
+      total += trip.cost
+    end
+
+    return total
+  end
 end
