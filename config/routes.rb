4bc237e3d5ac70ac0b88468a58bd572e20bc747f
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-07 14:37:18 -0700

    working through views and making sure html has header and main so layout/styles is applied

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index', as:'welcome' 
+  root 'welcome#index', as:'welcome'
+
   resources :drivers
   resources :passengers
-  resources :trips, except: [:index]
+  resources :trips
 
 end
