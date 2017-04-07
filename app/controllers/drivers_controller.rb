2e41f955172bec2aa304eb82fe0f41a510dcf23e
Theresa Manney <theresa.m.17@gmail.com>
2017-04-07 13:33:57 -0700

    Commented out controller stuff for drivers, created has_many and belongs_to to driver, passenger, and trip.rb

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -4,51 +4,51 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
-  def show
-    @result_driver = Driver.find(params[:id])
-  end
+  # def show
+  #   @result_driver = Driver.find(params[:id])
+  # end
+  #
+  # def new
+  #   @driver = Driver.new
+  # end
+  #
+  # def edit
+  #   @driver = Driver.find(params[:id])
+  # end
+  #
+  # def update
+  #   @driver = Driver.find(params[:id])
+  #   @driver.name = driver_params[:name]
+  #   @driver.vin = driver_params[:vin]
+  #   if @driver.save
+  #     redirect_to driver_path(@driver.id)
+  #   end
+  # end
+  #
+  # def create
+  #   @driver = Driver.create driver_params
+  #   redirect_to drivers_path
+  # end
+  #
+  # def destroy
+  #   Drivers.destroy(params[:id])
+  #   redirect_to drivers_path
+  # end
+  #
+  # def rating
+  # end
+  #
+  # def driver_trips
+  # end
+  #
+  # def earnings
+  #   #questions here - this possibly borrows from cost per trip in trips - the math logic might need to be in the model instead of the controller, not certain.
+  # end
 
-  def new
-    @driver = Driver.new
-  end
-
-  def edit
-    @driver = Driver.find(params[:id])
-  end
-
-  def update
-    @driver = Driver.find(params[:id])
-    @driver.name = driver_params[:name]
-    @driver.vin = driver_params[:vin]
-    if @driver.save
-      redirect_to driver_path(@driver.id)
-    end
-  end
-
-  def create
-    @driver = Driver.create driver_params
-    redirect_to drivers_path
-  end
-
-  def destroy
-    Drivers.destroy(params[:id])
-    redirect_to drivers_path
-  end
-
-  def rating
-  end
-
-  def driver_trips
-  end
-
-  def earnings
-    #questions here - this possibly borrows from cost per trip in trips - the math logic might need to be in the model instead of the controller, not certain.
-  end
-
-  private
-
-  def driver_params
-    params.require(:driver).permit(:name, :lic_plate, :vin)
-  end
+  # private
+  #
+  # def driver_params
+  #   params.require(:driver).permit(:name, :lic_plate, :vin)
+  # end
 
 end
