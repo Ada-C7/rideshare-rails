df9a848fb8b6f3275ca4d42a42d3cb1d66667187
jjousun <jjousun@gmail.com>
2017-04-07 13:06:26 -0700

    Making an edit page for riders, also added delete and home buttons

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -4,6 +4,27 @@ class RidersController < ApplicationController
   end
 
   def show
-    @rider = Rider.find(params[:id])  
+    @rider = Rider.find(params[:id])
+  end
+
+  def edit
+    @rider = Rider.find(params[:id])
+  end
+
+  def update
+    rider = Rider.find(params[:id])
+
+    rider.name = params[:rider][:name]
+    rider.phone_num = params[:rider][:phone_num]
+
+    if rider.save
+      redirect_to rider_path(rider.id)
+    end
+  end
+
+  def destroy
+    Rider.destroy(params[:id])
+
+    redirect_to riders_path
   end
 end
