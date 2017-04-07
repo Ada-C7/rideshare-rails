d8d62a2f33d721fe5cbcc2d04b1c20401bfa58eb
Andrea Valliere <amvalliere@gmail.com>
2017-04-07 15:31:09 -0700

    Began validations for trip. Not completed

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  validates :trip_id, length: { minimum: 1 }, uniqueness: true, numericality: { only_integer: true }
+  validates :driver_id, length: { minimum: 1 }, uniqueness: true, numericality: { only_integer: true }
+  validates :passenger_id, length: { minimum: 1 }, uniqueness: true, numericality: { only_integer: true }
+  validates :date, presence: true
+  validates_inclusion_of :rating, :in => 1..5
 end
