3e315793c4b0209b53ce2d709c506ea5b70ba4be
Andrea Valliere <amvalliere@gmail.com>
2017-04-07 00:18:32 -0700

    Added create and new methods for driver in drivers_controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,6 +3,16 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    puts ">>>> AMV: In TasksController#create"
+    Driver.create(driver_params)
+    redirect_to drivers_path
+  end
+
   def show
     @driver = Driver.find(params[:id])
   end
