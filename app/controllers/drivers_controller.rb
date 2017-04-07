cfff612af600fa13621794db248776313960e705
janicewilson <janice.wilson@mac.com>
2017-04-06 21:49:02 -0700

    Added controller actions, views and buttons for edit, update and delete.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -21,6 +21,32 @@ class DriversController < ApplicationController
           end
      end
 
+     def edit
+       @driver = Driver.find(params[:id])
+     end
+
+     def update
+          @driver = Driver.find(params[:id])
+
+          @driver.name = driver_params[:name]
+          @driver.vin = driver_params[:vin]
+          @driver.status = driver_params[:status]
+
+          if @driver.save
+               redirect_to driver_path
+          else
+               render 'edit'
+          end
+     end
+
+     def destroy
+       Driver.destroy(params[:id])
+
+       redirect_to drivers_path
+     end
+
+     private
+
      def driver_params
        params.require(:driver).permit(:name, :vin, :status)
      end
