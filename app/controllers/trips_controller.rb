3cda0811bea982052956fade0f254a0fdd627106
jjousun <jjousun@gmail.com>
2017-04-06 21:33:47 -0700

    Created an edit button for Trips index page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,22 @@
 class TripsController < ApplicationController
-    def index
-      @trips = Trip.all
+  def index
+    @trips = Trip.all
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    trip = Trip.find(params[:id])
+
+    trip.driver_id = params[:trip][:driver_id]
+    trip.rider_id = params[:trip][:rider_id]
+    trip.cost = params[:trip][:cost]
+    trip.rating = params[:trip][:rating]
+
+    if trip.save
+      redirect_to trips_path
     end
+  end
 end
