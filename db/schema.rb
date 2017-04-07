696ccd7557fdbb91ba3f8ab814b56e59082bdafa
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-07 16:58:32 -0700

    commented out drivers and passengers schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,19 @@ ActiveRecord::Schema.define(version: 20170405220245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "drivers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "vin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "passengers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "phone_num"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # create_table "drivers", force: :cascade do |t|
+  #   t.string   "name"
+  #   t.string   "vin"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+  #
+  # create_table "passengers", force: :cascade do |t|
+  #   t.string   "name"
+  #   t.string   "phone_num"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
 
   create_table "trips", force: :cascade do |t|
     t.integer  "driver_id"
