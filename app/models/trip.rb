0456b1ef48a657141fd8288db1efda5f968f8e14
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-08 12:49:02 -0700

    fixed create trip rating error. Now all new trips start with 0 rating

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,5 +5,5 @@ class Trip < ApplicationRecord
   validates :driver_id, presence: true
   validates :passenger_id, presence: true
   validates :date, presence: true
-  validates :rating, presence: true, inclusion: { in: (1..5) }
+  validates :rating, presence: true, inclusion: { in: (0..5) }
 end
