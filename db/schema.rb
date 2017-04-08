e9f4bec04051e747bbbedaf6fbd1d0435064d1b8
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-08 13:18:29 -0700

    made default cost for new trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407152826) do
+ActiveRecord::Schema.define(version: 20170408195852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 20170407152826) do
     t.integer  "passenger_id"
     t.string   "date"
     t.integer  "rating"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.float    "cost"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.float    "cost",         default: 0.0
   end
 
 end
