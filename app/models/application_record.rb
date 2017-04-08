53258a7445e963492bcc785db8d7983b5c75e4a0
Janice Lichtman <jalichtman@gmail.com>
2017-04-08 16:53:55 -0700

    replaced earnings method and spent method in driver and passenger models with total_money method in genral application model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def total_money
+    trips.empty? ? 0 : trips.map {|trip| trip.cost}.reduce(:+)
+  end
+
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,10 +4,6 @@ class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, presence: true, length: {is:17}, format: {with: /\A[a-zA-Z0-9]+\Z/}
 
-  def earnings
-    trips.empty? ? 0 : trips.map {|trip| trip.cost}.reduce(:+)
-  end
-
   def ave_rating
     return nil if trips.length == 0
     trips.map {|trip| trip.rating}.reduce(:+)/trips.length.to_f
