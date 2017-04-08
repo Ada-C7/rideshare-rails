cee41aee87ec483acb80d33200bd31c3015770bb
Janice Lichtman <jalichtman@gmail.com>
2017-04-08 16:48:26 -0700

    added earnings method to driver model and display of earningd to driver show page

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,6 +4,9 @@ class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, presence: true, length: {is:17}, format: {with: /\A[a-zA-Z0-9]+\Z/}
 
+  def earnings
+    trips.empty? ? 0 : trips.map {|trip| trip.cost}.reduce(:+)
+  end
 
   def ave_rating
     return nil if trips.length == 0
