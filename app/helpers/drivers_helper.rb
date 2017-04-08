cbf6f9fd74d5b2111ca9455aae8b1174235ef59a
Janice Lichtman <jalichtman@gmail.com>
2017-04-08 16:43:30 -0700

    changed display_ave_rating view helper to state a driver has had no trips yet when appropriate

--- app/helpers/drivers_helper.rb
+++ app/helpers/drivers_helper.rb
@@ -1,8 +1,9 @@
 module DriversHelper
 
   def display_ave_rating
-    return  "#{@driver.name} has not yet been rated"  if @driver.ave_rating.nil?
-    '%.2f' % @driver.ave_rating
+
+    return  "#{@driver.name.capitalize} has had no trips so far."  if @driver.ave_rating.nil?
+    "Average rating: #{'%.2f' % @driver.ave_rating}"
   end
 
 end
