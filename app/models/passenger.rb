f6a889dffd8ee2ab6a29652555d03cd0f0872d8f
Maria McGrew <mcg.maria@gmail.com>
2017-04-08 14:01:18 -0700

    changed trips csv drivers 83, 88, 154

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,3 @@
 class Passenger < ApplicationRecord
   has_many :trips
-
-  # validates :name, presence: true, uniqueness: true
-  # validates :passenger_id, length: { minimum: 1 }, uniqueness: true, numericality: { only_integer: true }
-  # validates :phone_num, presence: true
 end
