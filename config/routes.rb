90373f748f3fe833550e24222de0fc027a46ce30
Olivia <ssecretsharer@gmail.com>
2017-04-07 17:13:15 -0700

    nested some resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :passengers
-  resources :drivers
-  resources :trips
-
+  resources :passengers do
+      resources :trips
+    end
+    resources :drivers do
+      resources :trips
+  end
 end
