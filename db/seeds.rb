6c672ae4dc974d99eb8546bf37e6209f0ad8bd39
add2point71dots <alcland@gmail.com>
2017-04-07 21:43:38 -0700

    fixed a bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,74 +1,65 @@
 require 'csv'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-
-drivers = []
-CSV.read("../support/drivers.csv"[1..-1]).each do |line|
-  drivers << {
+drivers = CSV.read("support/drivers.csv")[1..-1].map do |line|
+  {
     name: line[1],
     vin: line[2],
     status: (line[3] == "true" ? true : false)
   }
 end
 
-driver_successes = 0
-drivers.each do |driver|
-  driver = Driver.create(driver)
-  if driver.id
-    driver_successes += 1
-  else
-    puts "FAILED ON #{driver[:name]}"
-  end
-end
 
-puts "Created #{driver_successes} out of #{drivers.length} drivers."
-
-
-passengers = []
-CSV.read("../support/passengers.csv"[1..-1]).each do |line|
-  passengers << {
+passengers = CSV.read("support/passengers.csv")[1..-1].map do |line|
+  {
     name: line[1],
     phone: line[2],
   }
 end
 
-passenger_successes = 0
-passengers.each do |passenger|
-  passenger = Passenger.create(passenger)
-  if passenger.id
-    passenger_successes += 1
-  else
-    puts "FAILED ON #{passenger[:name]}"
-  end
-end
 
-puts "Created #{passenger_successes} out of #{passengers.length} passengers."
-
-trips = []
-CSV.read("../support/trips.csv"[1..-1]).each do |line|
-  trips << {
-    driver_id: line[1].to_i,
-    passenger_id: line[2].to_i,
+trips = CSV.read("support/trips.csv")[1..-1].map do |line|
+  {
+    driver_id: line[1],
+    passenger_id: line[2],
     date: line[3],
-    rating: line[4].to_i,
-    cost: line[5].to_f
+    rating: line[4],
+    cost: line[5]
   }
 end
 
-trip_successes = 0
-trips.each do |trip|
-  trip = Trip.create(trip)
-  if trip.id
-    trip_successes += 1
-  else
-    puts "FAILED ON Trip with cost #{trip[:cost]}"
-  end
-end
-
-puts "Created #{trip_successes} out of #{trips.length} trips."
+# #uncomment to keep track of which lines didn't pass validation
+# driver_successes = 0
+# drivers.each do |driver|
+#   driver = Driver.create(driver)
+#   if driver.id
+#     driver_successes += 1
+#   else
+#     puts "FAILED ON #{driver[:name]}"
+#   end
+# end
+#
+# puts "Created #{driver_successes} out of #{drivers.length} drivers."
+#
+# passenger_successes = 0
+# passengers.each do |passenger|
+#   passenger = Passenger.create(passenger)
+#   if passenger.id
+#     passenger_successes += 1
+#   else
+#     puts "FAILED ON #{passenger[:name]}"
+#   end
+# end
+#
+# puts "Created #{passenger_successes} out of #{passengers.length} passengers."
+#
+# trip_successes = 0
+# trips.each do |trip|
+#   trip = Trip.create(trip)
+#   if trip.id
+#     trip_successes += 1
+#   else
+#     puts "FAILED ON Trip with cost #{trip[:cost]}"
+#   end
+# end
+#
+# puts "Created #{trip_successes} out of #{trips.length} trips."
