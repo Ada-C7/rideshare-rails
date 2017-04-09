bdcce1ab08a4024c313c6eb9f1fb37e2af126ef4
Lynn Trickey <lynn.trickey@gmail.com>
2017-04-09 15:00:19 -0700

    Fixed validations so they could read in csv files.  Fixed CSV files to remove ids of 0.  Added table for trip index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,24 @@
 require 'csv'
 
 
-driver_array = []
+# driver_array = []
 CSV.open('support/drivers.csv', 'r', :headers => true).each do |line|
-  name = line["name"].to_s
-  vin = line["vin"].to_s
+  name = line["name"]
+  vin = line["vin"]
 
-  driver_array << {name: name, vin: vin, availability: true}
+  # driver_array << {name: name, vin: vin, availability: true}
+  Driver.create({name: name, vin: vin, availability: true})
 end
 
-Driver.create(driver_array)
-
-rider_array = []
+# rider_array = []
 CSV.open('support/riders.csv', 'r', :headers => true).each do |line|
   name = line["name"].to_s
   phone_num = line["phone_num"].to_s
 
-  rider_array << {name: name, phone_num: phone_num}
+  Rider.create({name: name, phone_num: phone_num})
 end
 
-Rider.create(rider_array)
-
-trip_array = []
+# trip_array = []
 CSV.open('support/trips.csv', 'r', :headers => true).each do |line|
   driver_id = line["driver_id"].to_i
   rider_id = line["rider_id"].to_i
@@ -36,7 +33,6 @@ CSV.open('support/trips.csv', 'r', :headers => true).each do |line|
   rating = line["rating"].to_i
 
   cost = (rand(0.00...100.00)).round(2)
-  trip_array << {driver_id: driver_id, rider_id: rider_id, date: date, rating: rating, cost: cost }
+  {driver_id: driver_id, rider_id: rider_id, date: date, rating: rating, cost: cost }
+  Trip.create({driver_id: driver_id, rider_id: rider_id, date: date, rating: rating, cost: cost })
 end
-
-Trip.create(trip_array)
