0a5adc88aef4372e3cc7aae2b4a78e0d81ab15b5
Ashtn <ashtonehorton@gmail.com>
2017-04-08 19:24:40 -0700

    removed duplicate column, i think

--- db/migrate/20170404214212_add_user_columns.rb
+++ db/migrate/20170404214212_add_user_columns.rb
@@ -1,7 +1,7 @@
 class AddUserColumns < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :phone_num, :string
+    # add_column :users, :name, :string
+    # add_column :users, :phone_num, :string
     add_column :trips, :driver_id, :integer
     add_column :trips, :user_id, :integer
     add_column :trips, :rating, :integer
